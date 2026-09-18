b745ccdc6253795466bc0a5d32ca3ba3c8a9157d
Lesson Starter <lesson-starter@example.invalid>
2026-09-18 20:57:18 +0000

    chore: initialise Spring Boot lesson 1 project

---
+++ src/main/java/com/impact/ecommerce/Lesson1BackendApplication.java
@@ -0,0 +1,11 @@
+package com.impact.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson1BackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson1BackendApplication.class, args);
+    }
+}
